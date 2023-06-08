f96eb9f8d721fbc5ff9489e1ba73d774fe887132
facundo <facundojoaquinn@gmail.com>
2023-06-08 15:50:52 -0300

    Login y nav de la radio

---
+++ src/routes/+page.svelte
@@ -0,0 +1,126 @@
+<script>
+
+    let userInputValue = '';
+    let passwordInputValue = '';
+    function handleUserInputChange(event) {
+      userInputValue = event.target.value;
+    }
+  
+    function handlePasswordInputChange(event) {
+      passwordInputValue = event.target.value;
+    }
+  
+    function handleSubmit() {
+        //aca se puede desplegar la logica para checkear el user y paswrd
+      console.log('Usuario:', userInputValue);
+      console.log('Contraseña:', passwordInputValue);
+    }
+  </script>
+
+
+
+
+<section class="login">
+    <div class= "bgazul"></div>
+    <div class="login-container">
+        <div class="logincolor">
+            <img src="mic.png" alt="">
+        </div>
+        <div class="dform">
+            <h1>LOGIN</h1>
+            <form on:submit|preventDefault={handleSubmit}>
+                <label for="name"><img src="user.png" alt="user" class="user-label"></label>
+                <input type="text" bind:value={userInputValue} on:input={handleUserInputChange} name="user">
+                <label for="password"><img src="passwrd.png" class="user-label" alt="password"></label>
+                <input type="password" bind:value={passwordInputValue} on:input={handlePasswordInputChange} name="password">
+                <button type="submit">LOGIN</button>
+              </form>
+              
+        </div>
+    </div>
+</section>
+
+
+
+
+<style>
+    :global(body) { margin: 0; padding: 0;}
+    
+    h1 {
+        font-family: 'Roboto', sans-serif;
+        font-size: 96px;
+    }
+    .login {
+        
+        justify-content: center;
+        align-items: center;
+        width: 100vw;
+        height: 100vh;
+        position: relative;
+    }
+
+    .bgazul {
+        width: 34%;
+        height: 100vh;
+        background-color: #C4DFDF;
+    }
+    .login-container {
+        box-shadow: 0px 0px 38px 11px rgba(0, 0, 0, 0.25);
+        display: flex;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 70%;
+        height: 70%;
+        justify-content: space-between
+    }
+
+    .logincolor {
+        display: flex;
+        align-items: flex-end;
+        justify-content: center;
+        overflow: hidden;
+        width: 26%;
+        height: auto;
+        background-color: #C4DFDF;
+    }
+    .logincolor img {
+        height: 350px;
+        width: 350px;
+        transform: scaleX(-1);
+
+    }
+    .dform {
+        margin-right: 20%;
+    }
+    form {
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+    }
+    form input {
+        border: none;
+        border-bottom: 1px solid #c1c1c1;
+        outline: none; 
+        padding-left: 24px;
+}
+    input:hover {
+        border-bottom: 1px solid #6d6d6d;
+    }
+    .user-label {
+        position: absolute;
+        margin-top: 10px;
+    }
+    button {
+        height: 30px;
+        fill: none;
+        border-radius: 15px;
+        border: 2px solid #88dcdc
+    }
+    button:hover {
+        background-color: #C4DFDF;
+        transition: .3s;
+        color: white;
+    }
+</style>
---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,60 @@
+<nav class="navBar">
+    <ul>
+      <li>Dashboard</li>
+      <li>Clientes</li>
+      <li>Transmisión</li>
+      <li>Producción</li>
+      <li>Configuración</li>
+    </ul>
+  </nav>
+  
+  <style>
+    :global(body) {
+      margin: 0;
+      padding: 0;
+      margin-top: -16px;
+    }
+  
+    .navBar {
+      width: 100%;
+      box-shadow: 0px 0px 15px 0px rgba(0, 0, 0, 0.25);
+    }
+  
+    ul {
+      display: flex;
+      list-style: none;
+      gap: 20px;
+      height: 50px;
+      justify-content: center;
+    }
+  
+    li {
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      font-family: 'Roboto';
+      position: relative;
+      overflow: hidden;
+      margin: 0 10px;
+    }
+  
+    li::after {
+      content: '';
+      position: absolute;
+      left: 0;
+      top: -100%;
+      width: 100%;
+      height: 100%;
+      background-color: #C4DFDF;
+      transition: top 0.2s;
+      z-index: -1;
+      border-radius: 0px 0px 10px 10px;
+    }
+  
+    li:hover::after {
+      top: 0;
+    }
+  
+
+  </style>
+  
