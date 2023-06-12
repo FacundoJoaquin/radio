47d1e05895329ed96c040e68f827fdd5ad54a259
Lemonsix <alan.g.asc@gmail.com>
2023-06-12 11:24:13 -0300

    charts acomodados

---
+++ src/routes/components/organisms/Chart.svelte
@@ -0,0 +1,64 @@
+<script>
+    import { onMount } from "svelte";
+    import Chart from "chart.js/auto";
+    export let radios;
+    let chartInstance;
+    let id = "stackedChart";
+  
+    function createChart() {
+      const labels = radios.map((radio) => radio.name);
+      const freeSecondsData = radios.map((radio) => radio.freeSeconds);
+      const bookedSecondsData = radios.map((radio) => radio.bookedSeconds);
+  
+      const ctx = document.getElementById(id);
+  
+      chartInstance = new Chart(ctx, {
+        type: "bar",
+        data: {
+          labels,
+          datasets: [
+            {
+              label: "Segundos Libres",
+              data: freeSecondsData,
+              backgroundColor: "rgba(75, 192, 192, 0.2)",
+              borderColor: "rgba(75, 192, 192, 1)",
+              borderWidth: 1,
+            },
+            {
+              label: "Segundos Ocupados",
+              data: bookedSecondsData,
+              backgroundColor: "rgba(255, 99, 132, 0.2)",
+              borderColor: "rgba(255, 99, 132, 1)",
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+          scales: {
+            x: {
+              stacked: true,
+              ticks: {
+                // Update the tick marks to show as percentage
+                callback: function (value) {
+                  return (value * 100).toFixed(0) + "%";
+                },
+              },
+            },
+            y: {
+              stacked: true,
+              ticks: {
+                // Update the tick marks to show as percentage
+                callback: function (value) {
+                  return (value * 100).toFixed(0) + "%";
+                },
+              },
+            },
+          },
+        },
+      });
+    }
+    onMount(createChart);
+  </script>
+  
+  <canvas width="100%" height="100%" {id} />
+  
--- src/routes/components/organisms/StackedChart.svelte
+++ src/routes/components/organisms/StackedChart.svelte
@@ -1,58 +1,63 @@
 <script>
-  import { onMount } from 'svelte';
-  import Chart from 'chart.js/auto';
-  export let radio;
+  import { onMount } from "svelte";
+  import Chart from "chart.js/auto";
+  export let radios;
   let chartInstance;
-  let id = radio.name;
-
-  $: name = radio.name; // declaración reactiva
+  let id = "stackedChart";
 
   function createChart() {
+    const labels = radios.map((radio) => radio.name);
+    const freeSecondsData = radios.map((radio) => radio.freeSeconds);
+    const bookedSecondsData = radios.map((radio) => radio.bookedSeconds);
+
     const ctx = document.getElementById(id);
-    
+
     chartInstance = new Chart(ctx, {
-      type: 'bar',
+      type: "bar",
       data: {
-        labels: [name],
+        labels,
         datasets: [
           {
-            label: 'Free Seconds',
-            data: [radio.freeSeconds],
-            backgroundColor: 'rgba(75, 192, 192, 0.2)', // Color para freeSeconds
-            borderColor: 'rgba(75, 192, 192, 1)', // Border color para freeSeconds
-            borderWidth: 1
+            label: "Segundos Libres",
+            data: freeSecondsData,
+            backgroundColor: "rgba(75, 192, 192, 0.2)",
+            borderColor: "rgba(75, 192, 192, 1)",
+            borderWidth: 1,
           },
           {
-            label: 'Booked Seconds',
-            data: [radio.bookedSeconds],
-            backgroundColor: 'rgba(255, 99, 132, 0.2)', // Color para bookedSeconds
-            borderColor: 'rgba(255, 99, 132, 1)', // Border color para bookedSeconds
-            borderWidth: 1
+            label: "Segundos Ocupados",
+            data: bookedSecondsData,
+            backgroundColor: "rgba(255, 99, 132, 0.2)",
+            borderColor: "rgba(255, 99, 132, 1)",
+            borderWidth: 1,
           },
-        ]
+        ],
       },
       options: {
         scales: {
-          x:{
-            stacked: true
+          x: {
+            stacked: true,
+            ticks: {
+              // Update the tick marks to show as percentage
+              callback: function (value) {
+                return (value * 100).toFixed(0) + "%";
+              },
+            },
           },
           y: {
             stacked: true,
-            beginAtZero: true
-          }
-        }
-      }
+            ticks: {
+              // Update the tick marks to show as percentage
+              callback: function (value) {
+                return (value * 100).toFixed(0) + "%";
+              },
+            },
+          },
+        },
+      },
     });
   }
   onMount(createChart);
 </script>
 
-<div>
-<canvas id={id}></canvas>
-</div>
-<style>
-  div {
-    height: 400px;
-    width: 400px;
-  }
-</style>
+<canvas width="100%" height="100%" {id} />
